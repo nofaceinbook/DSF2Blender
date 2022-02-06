d434214e82354798f3ebcc756c63d1f46a99c606
schmax <schmax@gmx.de>
2022-02-06 12:15:17 +0100

    Update dsf_import_with_less_layers.py

--- dsf_import_with_less_layers.py
+++ dsf_import_with_less_layers.py
@@ -134,6 +134,22 @@ grid_south = int(dsf.Properties["sim/south"])
 print("Importing Mesh and setting west={} and south={} to origin.".format(grid_west, grid_south))
 
 
+# SORT mesh patches so that pyhiscal mesh is bottom layer (id in [0:10000]) and all overlys are above
+# All layers sorted based on the id of terrain in list, so that they will get higher z-value to avoid same z-layer artefacts
+# In addition this sorting allows to switch materials with every layer
+######## TBD: Give option to avoid loading of overlays
+ter_layers = dict()
+MAX_BASEMESH_TERRAINS = 10000  # number of allowed different terrains in basemesh, below this id we have basemesh, above overlays
+for p in dsf.Patches:
+    if p.flag == 1:  # Physical base mesh
+        layer_id = p.defIndex
+    else:
+        layer_id = p.defIndex + MAX_BASEMESH_TERRAINS 
+    if layer_id in ter_layers:
+        ter_layers[layer_id].append(p)
+    else:
+        ter_layers[layer_id] = [p]
+
     
 verts = []
 edges = []  # will not be filled as Blender takes in case of empty edges the edges from the faces
@@ -143,100 +159,117 @@ uvs = [[]]
 uvs2 = [[]] # second uv coordinates given for borders in case of non projected mesh
 coords = dict()  # existing coordinates per object as key and index of them in verts as value
 tria_layer = dict()  # returning for a tria of vertex index the current layer (how many are above eahc ohter)
-materials = dict()  # containing for each material = terrain the index
+#materials = dict()  # containing for each material = terrain the index
 matIndexPerTria = [[]] # list of material index for each tria of mesh
 used_materials = [[]]  # list containing for each layer the used materials
 terrain_details = dict()  # containing per terrain index the details of .ter-file in dict
 
-for p in dsf.Patches:
-    trias = p.triangles()
-    terrain = dsf.DefTerrains[p.defIndex]
-    if not p.defIndex in terrain_details:
-        terrain_details[p.defIndex] = read_ter_file(terrain, xp_path, dsf_file)
-        if "ERROR" in terrain_details[p.defIndex]:
-            print(terrain_details[p.defIndex]["ERROR"])
-    if "PROJECTED" in terrain_details[p.defIndex] or (terrain == "terrain_Water" and len(dsf.V[trias[0][0][0]][trias[0][0][1]]) <= 5):
-        # if water is projected depends if uv coordinates are given or not, taken from ferst vertex in first tria
-        ##### TBD: Can cause issue in case patch is empty
+for ter_layer_id in sorted(ter_layers.keys()): 
+    ######## For new ter_layer we have to load terain details and create according material ##############
+    if ter_layer_id > MAX_BASEMESH_TERRAINS:
+        ter_defIndex = ter_layer_id - MAX_BASEMESH_TERRAINS 
+        terrain = dsf.DefTerrains[ter_defIndex] + "_OVL"
+    else:
+        ter_defIndex = ter_layer_id 
+        terrain = dsf.DefTerrains[ter_defIndex]
+    if not ter_defIndex in terrain_details:  ##### TBD: DIRECTLY READ ALL TERRAIN DETAILS AT BEGINNING ####
+        terrain_details[ter_defIndex] = read_ter_file(terrain, xp_path, dsf_file)
+        if "ERROR" in terrain_details[ter_defIndex]:
+            print(terrain_details[ter_defIndex]["ERROR"])
+    if "PROJECTED" in terrain_details[ter_defIndex]:
         projected_uv = True
     else:
         projected_uv = False
-    if p.flag > 1:  # this is an overlay
-        terrain += "_OVL"
-    #### TBD: INCLDUDE NEAR/FAR VALUES FOR PATCH TO MATERIAL #####
-    if terrain in materials:
-        m = materials[terrain]  ######### TBD: this with used_materials seems not efficient ######
-    else:
-        m = add_material(terrain, terrain_details[p.defIndex], bpy)  # add material to Blender materials
-        materials[terrain] = m
-    for t in trias:
-        ti = []  # index list of vertices of tria that will be added to faces
-        tuvs = []  # uvs for that triangle
-        tuvs2 = []  # 2nd uves for triangle e.g. for borders if existent
-        for v in t:  # this is now index to Pool and to vertex in Pool
-            #### TBD: Scale to Marcartor in order to have same east/west and north/south dimension #####
-            vx = round((dsf.V[v[0]][v[1]][0] - grid_west) * 1000, 3)
-            vy = round((dsf.V[v[0]][v[1]][1] - grid_south) * 1000, 3)
-            vz = dsf.getVertexElevation(dsf.V[v[0]][v[1]][0], dsf.V[v[0]][v[1]][1], dsf.V[v[0]][v[1]][2])
-            vz = round(vz / 100, 3)  ### TBD: Make stretching of height configureable
-            if (vx, vy) in coords:
-                vi = coords[(vx, vy)]
-                #### TBD: check if new normal is equal to existing one ###############
+    m = add_material(terrain, terrain_details[ter_defIndex], bpy)  # add material to Blender materials
+    #### TBD: INCLDUDE NEAR/FAR VALUES FOR PATCH TO MATERIAL ##### ---> PERHAPS ALREADY IN TER_LAYER CREATION
+    #### TBD: Add to material name "PRJ" if it is projected without actually having uvs, would help export later not to read ter file ##
+     
+    #if terrain in materials: ##### TBD: Dictionary not needed any more, when moved above to change of layer_id = change of material
+    #    m = materials[terrain]  ######### TBD: this with used_materials seems not efficient ######
+    #else:
+    #    m = add_material(terrain, terrain_details[p.defIndex], bpy)  # add material to Blender materials
+    #    materials[terrain] = m
+    
+    for p in ter_layers[ter_layer_id]:
+        trias = p.triangles()
+        
+        if len(trias) and terrain == "terrain_Water" and len(dsf.V[trias[0][0][0]][trias[0][0][1]]) <= 5:
+            projected_uv = True
+        # if water is projected depends if uv coordinates are given or not, taken from ferst vertex in first tria
+
+        for t in trias:
+            ti = []  # index list of vertices of tria that will be added to faces
+            tuvs = []  # uvs for that triangle
+            tuvs2 = []  # 2nd uves for triangle e.g. for borders if existent
+            for v in t:  # this is now index to Pool and to vertex in Pool
+                #### TBD: Scale to Marcartor in order to have same east/west and north/south dimension #####
+                vx = round((dsf.V[v[0]][v[1]][0] - grid_west) * 1000, 3)
+                vy = round((dsf.V[v[0]][v[1]][1] - grid_south) * 1000, 3)
+                vz = dsf.getVertexElevation(dsf.V[v[0]][v[1]][0], dsf.V[v[0]][v[1]][1], dsf.V[v[0]][v[1]][2])
+                vz = round(vz / 100, 3)  ### TBD: Make stretching of height configureable
+                if (vx, vy) in coords:
+                    vi = coords[(vx, vy)]
+                    #### TBD: check if new normal is equal to existing one ###############
+                else:
+                    vi = len(coords)  # index in verts is the last one, as coords will now be added
+                    coords[(vx, vy)] = vi 
+                    verts.append([vx, vy, vz])
+                    nx = round(dsf.V[v[0]][v[1]][3], 4)  #### TBD: Rounding and if below can be removed; just checking if existent
+                    ny = round(dsf.V[v[0]][v[1]][4], 4)
+                    normals.append([nx, ny, round(sqrt(1 - nx*nx - ny*ny), 4)])
+                    #if normals[-1] != [0.0, 0.0, 1.0]:
+                    #    print(normals[-1])
+                ti.insert(0, vi)  # winding in Blender is just opposite as in X-Plane
+                if len(dsf.V[v[0]][v[1]]) == 7:  # in case of projection we need first uvs do by own unwrapping and use the others as second e.g. for border
+                    if not projected_uv and p.flag == 1:  # for projected physical mesh; for overlay we would need second uvs for border
+                        ########### TBD: when projected then map tuvs to vx and vy --> if NOT projected, CORRRECT ????????? ################################
+                        tuvs.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))
+                        tuvs2.insert(0, (vx/100, vy/100))  # add this uv, even if not needed in order to get full uv-mesh for that layer
+                    else:  # should only be the case if projected and we have overlay to get uv-map for border
+                        tuvs.insert(0, (vx/100, vy/100))  #By this definition uvs exced [0;1] range, but should lead to scale 10 times the size
+                        tuvs2.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))  # uvs are defined for every vertex of every face / loop
+                elif len(dsf.V[v[0]][v[1]]) == 9: # first uvs for mesh 2nd for border
+                    tuvs.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))  # uvs are defined for every vertex of every face / loop
+                    tuvs2.insert(0, (dsf.V[v[0]][v[1]][7], dsf.V[v[0]][v[1]][8]))  # uvs are defined for every vertex of every face / loop                    
+                else: # we don't have uvs so we unwrap our own ones
+                    tuvs.insert(0, (vx/100, vy/100))  # By this definition uvs exced [0;1] range, but should lead to scale 10 times the size
+                    tuvs2.insert(0, (vx/100, vy/100))  # Add uvs even if not needed for that tria but to have full uvs for that layer 
+            smallest_index = min(ti)  # make sure that smallest index is first in the list, but keep winding of tria
+            if smallest_index == ti[1]:
+                ti_match = (ti[1], ti[2], ti[0])  ########  CHANGING ORDER WOULD MEAN ALSO TO CHANG ORDER FOR UV --> created ti just for matching in dict !!!!!!! ##########
+            elif smallest_index == ti[2]:
+                ti_match = (ti[2], ti[0], ti[1])
             else:
-                vi = len(coords)  # index in verts is the last one, as coords will now be added
-                coords[(vx, vy)] = vi 
-                verts.append([vx, vy, vz])
-                nx = round(dsf.V[v[0]][v[1]][3], 4)  #### TBD: Rounding and if below can be removed; just checking if existent
-                ny = round(dsf.V[v[0]][v[1]][4], 4)
-                normals.append([nx, ny, round(sqrt(1 - nx*nx - ny*ny), 4)])
-                #if normals[-1] != [0.0, 0.0, 1.0]:
-                #    print(normals[-1])
-            ti.insert(0, vi)  # winding in Blender is just opposite as in X-Plane
-            if len(dsf.V[v[0]][v[1]]) == 7:  # in case of projection we need first uvs do by own unwrapping and use the others as second e.g. for border
-                if not projected_uv and p.flag == 1:  # for projected physical mesh; for overlay we would need second uvs for border
-				    ########### TBD: when projected then map tuvs to vx and vy --> if NOT projected, CORRRECT ????????? ################################
-                    tuvs.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))
-                    tuvs2.insert(0, (vx/100, vy/100))  # add this uv, even if not needed in order to get full uv-mesh for that layer
-                else:  # should only be the case if projected and we have overlay to get uv-map for border
-                    tuvs.insert(0, (vx/100, vy/100))  #By this definition uvs exced [0;1] range, but should lead to scale 10 times the size
-                    tuvs2.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))  # uvs are defined for every vertex of every face / loop
-            elif len(dsf.V[v[0]][v[1]]) == 9: # first uvs for mesh 2nd for border
-                tuvs.insert(0, (dsf.V[v[0]][v[1]][5], dsf.V[v[0]][v[1]][6]))  # uvs are defined for every vertex of every face / loop
-                tuvs2.insert(0, (dsf.V[v[0]][v[1]][7], dsf.V[v[0]][v[1]][8]))  # uvs are defined for every vertex of every face / loop                    
-            else: # we don't have uvs so we unwrap our own ones
-                tuvs.insert(0, (vx/100, vy/100))  # By this definition uvs exced [0;1] range, but should lead to scale 10 times the size
-                tuvs2.insert(0, (vx/100, vy/100))  # Add uvs even if not needed for that tria but to have full uvs for that layer 
-        smallest_index = min(ti)  # make sure that smallest index is first in the list, but keep winding of tria
-        if smallest_index == ti[1]:
-            ti_match = (ti[1], ti[2], ti[0])  ########  CHANGING ORDER WOULD MEAN ALSO TO CHANG ORDER FOR UV --> created ti just for matching in dict !!!!!!! ##########
-        elif smallest_index == ti[2]:
-            ti_match = (ti[2], ti[0], ti[1])
-        else:
-            ti_match = (ti[0], ti[1], ti[2])
-        if ti_match in tria_layer:  # this tria is already existing
-            tria_layer[ti_match] += 1  # this new tria has to be pot on next layer
-            layer = tria_layer[ti_match]
-        else:
-            tria_layer[ti_match] = 0  # this is first tria which is layer 0 (base mesh)
-            layer = 0    
-        if layer >= len(faces):  # We need addtional layer so extend lists
-            faces.append([])
-            uvs.append([])
-            uvs2.append([])
-            matIndexPerTria.append([]) 
-            used_materials.append([])
-        faces[layer].append(ti)
-        uvs[layer].extend(tuvs)  # uvs added with correct order because of tria different winding
-        if tuvs2 != []:
-            uvs2[layer].extend(tuvs2)
-        ########### TBD: used materials defined as dict per layer for better performance ###############
-        #print(m, used_materials[layer])
-        if m not in used_materials[layer]:
-            mat = len(used_materials[layer])
-            used_materials[layer].append(m)
-        else:
-            mat = used_materials[layer].index(m)
-        matIndexPerTria[layer].append(mat)    
+                ti_match = (ti[0], ti[1], ti[2])
+            if ti_match in tria_layer:  # this tria is already existing
+                tria_layer[ti_match] += 1  # this new tria has to be pot on next layer
+                layer = tria_layer[ti_match]
+            else:
+                tria_layer[ti_match] = 0  # this is first tria which is layer 0 (base mesh)
+                layer = 0    
+            if layer >= len(faces):  # We need addtional layer so extend lists
+                faces.append([])
+                uvs.append([])
+                uvs2.append([])
+                matIndexPerTria.append([]) 
+                used_materials.append([])
+            faces[layer].append(ti)
+            uvs[layer].extend(tuvs)  # uvs added with correct order because of tria different winding
+            if tuvs2 != []:
+                uvs2[layer].extend(tuvs2)
+            if len(used_materials[layer]) == 0 or m != used_materials[layer][-1]:  # as materials are sorted per layer_id we need just to check if required material is at end of the list
+                mat = len(used_materials[layer])
+                used_materials[layer].append(m)
+            else:
+                mat = used_materials[layer].index(m)
+            matIndexPerTria[layer].append(mat)    
+
+# Create own collection for basemesh and overlays
+main_collection = bpy.data.collections.new("XPDSF")
+bpy.context.scene.collection.children.link(main_collection)
+ol_collection = bpy.data.collections.new("Overlays")
+main_collection.children.link(ol_collection)
+
 
 for layer in range(len(faces)):
     if layer == 0:
@@ -247,14 +280,16 @@ for layer in range(len(faces)):
     mesh = bpy.data.meshes.new(mesh_name)  # add the new mesh
 
     obj = bpy.data.objects.new(mesh.name, mesh)
-    col = bpy.data.collections.get("Collection")
+    
+    if mesh_name.startswith("Base"):
+        col = bpy.data.collections.get("XPDSF")
+    else:
+        col = bpy.data.collections.get("Overlays")
+        
     col.objects.link(obj)
     bpy.context.view_layer.objects.active = obj
 
-    ##### Delete loose vertices #### TBD: NOT WORKING --> check bpy.ops usage or use bmesh.ops ###########
-    #bpy.ops.object.mode_set(mode='EDIT')
-    #bpy.ops.mesh.delete_loose()
-    #bpy.ops.object.mode_set(mode='OBJECT')
+    ##### Delete loose vertices #### 
     if layer > 0:
         verts_layer = []
         faces_layer = []
@@ -303,6 +338,6 @@ for layer in range(len(faces)):
     ### Move overlays along z-axis
     obj.location.z += layer * 0.01
 
-    #### TBD: Add to material name "PRJ" if it is projected without actually having uvs, would help export later not to read ter file ##
+   
 
         
